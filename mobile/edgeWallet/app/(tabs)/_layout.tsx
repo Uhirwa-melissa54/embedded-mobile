90dbdf9d514d2e8320067ffc83521552a17e1584
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2026-03-11 14:51:20 +0200

    Dashboard, topup and transactions..

--- mobile/edgeWallet/app/(tabs)/_layout.tsx
+++ mobile/edgeWallet/app/(tabs)/_layout.tsx
@@ -7,29 +7,13 @@ import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs>
+      <Tabs.Screen name = "dashboard" options={{title: "Dashboard"}} />
+      <Tabs.Screen name = "topup" options={{title: "Top Up"}} />
+      <Tabs.Screen name = "payment" options={{title: "Payment"}} />
+      <Tabs.Screen name = "transactions" options={{title: "History"}} />
     </Tabs>
   );
 }
